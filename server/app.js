f060a900ced78e24141c7427b57caee7f061411c
ttreit <14933277+ttreit@users.noreply.github.com>
2020-06-24 20:41:11 -0700

    Complete Adding Post Routes

--- server/app.js
+++ server/app.js
@@ -17,67 +17,122 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 
 
-app.get('/', 
-(req, res) => {
-  res.render('index');
-});
+app.get('/',
+  (req, res) => {
+    res.render('index');
+  });
 
-app.get('/create', 
-(req, res) => {
-  res.render('index');
-});
+app.get('/create',
+  (req, res) => {
+    res.render('index');
+  });
 
-app.get('/links', 
-(req, res, next) => {
-  models.Links.getAll()
-    .then(links => {
-      res.status(200).send(links);
-    })
-    .error(error => {
-      res.status(500).send(error);
-    });
-});
-
-app.post('/links', 
-(req, res, next) => {
-  var url = req.body.url;
-  if (!models.Links.isValidUrl(url)) {
-    // send back a 404 if link is not valid
-    return res.sendStatus(404);
-  }
-
-  return models.Links.get({ url })
-    .then(link => {
-      if (link) {
-        throw link;
-      }
-      return models.Links.getUrlTitle(url);
-    })
-    .then(title => {
-      return models.Links.create({
-        url: url,
-        title: title,
-        baseUrl: req.headers.origin
+app.get('/links',
+  (req, res, next) => {
+    models.Links.getAll()
+      .then(links => {
+        res.status(200).send(links);
+      })
+      .error(error => {
+        res.status(500).send(error);
       });
-    })
-    .then(results => {
-      return models.Links.get({ id: results.insertId });
-    })
-    .then(link => {
-      throw link;
-    })
-    .error(error => {
-      res.status(500).send(error);
-    })
-    .catch(link => {
-      res.status(200).send(link);
-    });
-});
+  });
+
+app.post('/links',
+  (req, res, next) => {
+    var url = req.body.url;
+    if (!models.Links.isValidUrl(url)) {
+      // send back a 404 if link is not valid
+      return res.sendStatus(404);
+    }
+
+    return models.Links.get({ url })
+      .then(link => {
+        if (link) {
+          throw link;
+        }
+        return models.Links.getUrlTitle(url);
+      })
+      .then(title => {
+        return models.Links.create({
+          url: url,
+          title: title,
+          baseUrl: req.headers.origin
+        });
+      })
+      .then(results => {
+        return models.Links.get({ id: results.insertId });
+      })
+      .then(link => {
+        throw link;
+      })
+      .error(error => {
+        res.status(500).send(error);
+      })
+      .catch(link => {
+        res.status(200).send(link);
+      });
+  });
 
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
 
+app.get('/signup', (req, res) => {
+  //call signup page
+  res.render('signup');
+});
+
+app.post('/signup', (req, res, next) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  return models.Users.get({ username })
+    .then((user) => {
+      if (!user) {
+        return models.Users.create({ username, password })
+          .then((user) => {
+            res.status(200).send('Written to database!');
+            //models.Sessions.create();
+          })
+          .catch((err) => {
+            console.log('ERROR: ', err);
+          });
+      } else {
+        //message to user?? Saying username taken
+        console.log('Username Taken');
+        res.redirect('/signup');
+      }
+    });
+});
+
+
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  return models.Users.get({ username })
+    .then((user) => {
+      if (!user) {
+        res.redirect('/login');
+      } else {
+        return models.Users.compare(password, user.password, user.salt); //returns boolean true if passwords match.
+      }
+    })
+    .then((authenticated) => {
+      if (authenticated) {
+        console.log('Logged in!');
+        res.redirect('/');
+        //TODO How do we redirect user to personal page?
+          //Sessions.isLoggedIn?
+      } else {
+        res.redirect('/login');
+      }
+    });
+});
 
 
 /************************************************************/

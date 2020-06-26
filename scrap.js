1eb67da110529102d77db5f68923c8bb5bdc3347
ttreit <14933277+ttreit@users.noreply.github.com>
2020-06-26 13:02:18 -0700

    Add file for code we may use during sprint

---
+++ scrap.js
@@ -0,0 +1,10 @@
+//**************************** */
+//TEST DELETE THIS
+//// parseCookies
+
+app.get('/testcookie', (req, res) => {
+  let test = cookie(req, res);
+  console.log('TEST****: ', test);
+});
+
+//**************************** */

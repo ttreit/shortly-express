78b8cccf9376543a41d5eaa122e3cde4ab652054
ttreit <14933277+ttreit@users.noreply.github.com>
2020-06-26 21:17:34 -0700

    Refactor to attach cookie object to req

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,17 +1,26 @@
 const parseCookies = (req, res, next) => {
   //req.headers.cookie //req.cookie
   var cookieObj = {};
-  var cookies = req.headers.cookie.split('; ');
-  //loop through cookies arr
-  for (var i = 0; i < cookies.length; i++) {
+  if (!req.headers.cookie) {
+    //return {}; add empty object to req
+    req.cookies = cookieObj;
+    next();
+  } else {
+    var cookies = req.headers.cookie.split('; ');
+    //loop through cookies arr
+    for (var i = 0; i < cookies.length; i++) {
     //obj keys
-    var cookieKeyValue = cookies[i].split('=');
-    cookieObj[cookieKeyValue[0]] = cookieKeyValue[1];
+      var cookieKeyValue = cookies[i].split('=');
+      cookieObj[cookieKeyValue[0]] = cookieKeyValue[1];
+    }
+    req.cookies = cookieObj;
+    next();
   }
-  return cookieObj;
-  next();
-};
 
+  //return cookieObj; //add to req obj and to res obj in Auth
+};
+//if line 9
+//else line 4 w/ req.cookies = cookieObj
 
 module.exports = parseCookies;
 

c7ab313cdeb6f685127139eaf309d59ccd0f4f0f
ttreit <14933277+ttreit@users.noreply.github.com>
2020-06-26 17:33:37 -0700

    Create auth middleware - in progress

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,9 +2,40 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  //console.log('SHORTLY :', req.cookies.shortlyId);
+  console.log('REQ :', req);
+  console.log('COOKIES :', req.headers.cookie);
+  console.log('REQSESSION: ', req.session);
+  next();
+
+
+  //get parsed cookie
+  //check if there is a session hash - if no, or if invalid create hash, send to server in req and res objects
+  //if valid hash - send to server in req and res objects
 };
+
+//NOTES FROM TEST
+//initializes a new session when there are no cookies on the request
+  //req.cookies.shortlyId
+  //cookies['shortlyid'] must exist
+
+//sets a new cookie on the response when a session is initialized
+  //req.cookies.shortlyId
+
+//assigns a session object to the request if a session already exists
+
+//creates a new hash for each new session
+
+//assigns a username and userId property to the session object if the session is assigned to a user
+
+//clears and reassigns a new cookie if there is no session assigned to the cookie
+
+
+
 
 /************************************************************/
 // Add additional authentication middleware functions below
 /************************************************************/
+
+
 
